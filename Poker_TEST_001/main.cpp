8d205e62a41bf60749ee0d1e9110ab414a9e83e6
mistermaxis <55718118+mistermaxis@users.noreply.github.com>
2019-11-16 19:56:15 -0300

    Add project files.

---
+++ Poker_TEST_001/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+
+int main()
+{
+	Table table1;
+	Player player1;
+	Dealer dealer1;
+	std::cout << table1.showCard().card << "\n";
+	std::cout << player1.showCard().card << "\n";
+	dealer1.Deal(table1);
+	dealer1.Deal(player1);
+	std::cout << table1.showCard().card << "\n";
+	std::cout << player1.showCard().card << "\n";
+
+	return 0;
+}
---
+++ Poker_TEST_001/pch.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+class Card {
+public:
+	unsigned int card;
+	Card() { card = 0; }
+};
+
+class IDealable {
+public:
+	virtual void Deal(Card c) = 0;
+};
+
+class Dealable : public IDealable {
+private:
+	Card m_card;
+public:
+	virtual void Deal(Card c) { m_card.card = c.card; }
+	Card showCard() { return m_card; }
+};
+
+class Table : public Dealable {
+
+};
+
+class Player : public Dealable {
+
+};
+
+class Dealer {
+public:
+	void Deal(Dealable& d)
+	{
+		srand(time(nullptr));
+		Card card_;
+		card_.card = rand() % 52;
+		d.Deal(card_);
+	}
+};
